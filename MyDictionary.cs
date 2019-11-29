39a3b6cfb17388c8aa9ed46fecd2ef70537a2f4c
Michelangelo Marchesi <michelangelo.marchesi@gmail.com>
2019-11-29 19:21:43 +0200

    Add project files.

---
+++ MyDictionary.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace DictClean
+{
+    class MyDictionary
+    {
+        public List<string> wordlist { get; private set; }
+
+
+
+        // Create lists of strings for each column of the cvs file
+        // This file has nr. columns separated by a single blank
+        // Some line contains an extra blank
+        //  
+        // 7 86711 hän (0.4925 %)
+        // 8 78076 mutta (0.4434 %)
+        // 98657 13 2---0 1---0 (7.3842e-05 %)
+        /*
+         * first and second column end with a blank
+         * the third column ends before the opening parenthesis "("
+         * the fourth column ends with a closing parenthesis ")"
+         * 
+         */
+
+
+        // Load the entire word list (mydictionary) in memory
+        public List<string> LoadDictionary(string myfile, string separatedby)
+        {
+            using (var streamdata = new StreamReader(myfile, Encoding.GetEncoding("iso-8859-1")))
+            {
+                string strjoiner;
+                int j = 0;
+
+                List<string> wl = new List<string>();
+
+                while (!streamdata.EndOfStream)
+                {
+                    var readline = streamdata.ReadLine();
+
+                    var values = readline.Split(separatedby);
+
+                    // if the line has more than 3 blanks
+                    // so has more than 4 columns
+                   
+                    j = 2;
+                    //
+                    strjoiner = "";
+                    // I want the string contained in column 3 (having index 2) and more
+                    // except the last one (index Length - 1)
+                    do
+                    {
+                        strjoiner = strjoiner + values[j];
+
+                        j++;
+
+                    } while (j == (values.Length - 1));
+
+                                      
+
+                    wl.Add(strjoiner);
+                    
+
+                    
+                }
+
+                return wl;
+            }
+        }
+
+
+        // Write the entire word list (mydictionary) into a file
+
+
+
+        // Read line by line and remove numeric sequences
+        // 542522 1 450/87 4 (5.6802e-06 %)
+        // 542522 1 1990 54 (5.6802e-06 %)
+
+
+
+        // open csv dictionary file
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+/* Project DictClean
+ * by Michelangelo Marchesi
+ * start date:   28/11/2019
+ * last review:  29/11/2091
+ */
+
+/* Project specification 
+ * and to do list:
+ * 
+
+//Dictionary Normalizer
+// Given list of words in csv file (comma separated values)
+// returns a file that has been cleaned using regular expressions
+// removing errors in compilation of the list.
+// The check happens on a given column of the csv file.
+// The separator in the file can be decided (comma, point, space, tab)
+// The file is read, precessed for trimming and number removal
+// then is added an index and sorted by the given column
+// Some of the features:
+// - trimming non numeric and alphabetic beginners and enders (- / > (like: =d, heränneellä/, -pula-aika) deleting line if short remains
+// - purely numeric lines are removed (even if contain : , . / --- + or space, ex: 55a, 90 00o:n, 1992 21, 160000:een)
+// - text+num are removed (ex. tol9133, 90-luku)
+// - travel routes or sport are removed (gulko---bischoff, v4---arvo, ilves---lahden)
+// - url type words to be deleted (ex. http://, desc=href=http://)
+// - remove words containing ~ (ex. g~nter)
+// - remove words containing : (like fst:lle, usa:sa, ky:ssä, ap-dj:lle, gmbh:lta)
+// - remove abitation descrition like (2h+k+wc+kph)
+// - remove
+//
+//
+//
+
+/* Classes:
+ * Dictionary methods: 
+ * 
+ */
+
+
+namespace DictClean
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myfilepath = @"M:\MichelangeloMarchesi\_CodeDevelopment\source\repos\DictClean\Data\parole_frek_Fi_1339787_to_normalize.csv";
+            string myseparator = " ";
+
+            List<string> myWordList = new List<string>();
+
+            MyDictionary xm = new MyDictionary();
+
+            //test advice
+            Console.WriteLine("Loading dictionaryfile: {0}", myfilepath);
+
+            myWordList = xm.LoadDictionary(myfilepath, myseparator);
+
+            // test load printing few lines
+
+            for (int i=1; i<100; i++)
+            {
+                Console.Write(" {0}) " + myWordList[i * 10000] + "{1}", i*10000, ";");
+            }
+            Console.ReadKey();
+        }
+    }
+}
